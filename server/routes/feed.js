3c31c6ec32c74c481ac925ebc12161170f1a4464
Saurabh Kanhegaonkar <saurabh.kanhegaonkar@gmail.com>
2020-04-12 01:37:09 -0700

    Added queries to fetch the correct event data from the db

--- server/routes/feed.js
+++ server/routes/feed.js
@@ -4,12 +4,48 @@ let router = express.Router();
 import { Event } from '../models/event';
 
 
+const getLiveEvents = async (userLocation) => {
+  if (userLocation) { // user has given his location
+    // https://developers.google.com/web/fundamentals/native-hardware/user-location#determining_the_users_current_location
+    const { latitude, longitude } = userLocation.coords;
+
+    // send live events sorted by location, then ended events sorted by endTimestamp 
+    let now = new Date().toUTCString();
+    let sortedLiveEvents = await Event
+    .query()
+    .select()
+    .where('startTimestamp', '<', now)
+    .where('endTimestamp', '>', now)
+    .orderByRaw('ST_MakePoint(coordinate[0], coordinate[1]) <-> ST_MakePoint(?, ?)::geography', [longitude, latitude])
+    .debug();
+
+    return sortedLiveEvents;
+  }
+}
+
+const getEndedEvents = async () => {
+  let now = new Date().toUTCString();
+  const sortedEndedEvents = await Event
+  .query()
+  .select()
+  .where('startTimestamp', '>=', now)
+  .orWhere('endTimestamp', '<=', now)
+  .orderBy('endTimestamp', 'desc');
+
+  return sortedEndedEvents;
+}
+
 /* GET users listing. */
-router.get('/', async (req, res, next) => {
-  let events = await Event.query();
-  let liveEvents = events.filter((event) => event.isLive);
-  console.log('liveEvents: ', liveEvents.length);
-  res.send(liveEvents);
+router.get('/events', async (req, res, next) => {
+  let liveEvents = await getLiveEvents({
+    coords: {
+      longitude: -122.19561,
+      latitude: 47.82152
+    }
+  });
+
+  let endedEvents = await getEndedEvents();
+  res.send({events: [...liveEvents, ...endedEvents]});
 });
 
 export default router;
